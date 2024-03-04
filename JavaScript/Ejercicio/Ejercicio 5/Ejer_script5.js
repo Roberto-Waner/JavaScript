fb41c167397b06fbe2a2a2aac148a5cff02c1897
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2024-03-04 16:23:54 -0400

    Practica

--- JavaScript/Ejercicio/Ejercicio 5/Ejer_script5.js
+++ JavaScript/Ejercicio/Ejercicio 5/Ejer_script5.js
@@ -4,21 +4,45 @@
 //     c- Alumnos que aprobaron unicamente el ultimo examen.
 
 let estudiante, aprob_todo = 0, aprob_algunos = 0, aprob_ultimo = 0;
+function Estudiantes(estudiante){
+    estudiante = parseInt(prompt("Cantidad de Estudiantes a evaluar: "))
+    document.write(`Cantidad de Estudiantes a evaluar: ${estudiante} <br>`);
+    
+    for(let i = 0; i < estudiante; i++){
+        alert(`Ingrese las 3 notas del ${i+1}ro estudiante`)
+        NotasIngresadas();
+    }
+    
+    return estudiante;
+}
 
 function NotasIngresadas(){
     let nota = new Array(3)
 
-    for(let i = 0; i < nota.length; i++){
+    for(var i = 0; i < nota.length; i++){
         nota[i] = parseInt(prompt(`Ingresar la ${i + 1}ra nota del estudiante: `))
+    }
+    
+    Calificacion(nota);
+    return nota;
+}
+
+function Calificacion(nota){
+    if(nota[0] >= 70 && nota[1] >= 70 && nota[2] >= 70){
+        aprob_todo++
+    }else if(nota[0] >= 70 || nota[1] >= 70 || nota[2] >= 70){
+            aprob_algunos++
+    }
+    
+    if(nota[2] >= 70 && nota[0] < 70 && nota[1] < 70){
+        aprob_ultimo++
     }
 
     return nota;
 }
 
-function Estudiantes(){
-    estudiante = parseInt(prompt("Cantidad de Estudiantes a evaluar: "))
+Estudiantes(estudiante)
 
-    for(let i = 0; i < estudiante; i++){
-
-    }
-}
+document.write(`Total de los Alumnos que aprobaron todos los examenes ${aprob_todo}` + " <br>")
+document.write(`Total de los Alumnos que aprobaron al menos uno de los examenes ${aprob_algunos}` + "<br>")
+document.write(`Total de los Alumnos que solo aprobaron el ultimo examen ${aprob_ultimo} <br>`)
